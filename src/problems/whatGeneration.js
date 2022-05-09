fad2b8fb20f9fe337ca508b3de53f2f56b5e5132
scorcuera <sc@thehotelsnetwork.com>
2022-05-09 21:42:36 +0200

    feat(problems): add new testing case, whatGeneration

---
+++ src/problems/whatGeneration.js
@@ -0,0 +1,8 @@
+export function whatGeneration(year) {
+    // write your function here
+};
+
+// Hint:
+// Generation X: Born 1965-1980
+// Millennials: Born 1981-1996
+// Generation Z: Born 1997-2012
