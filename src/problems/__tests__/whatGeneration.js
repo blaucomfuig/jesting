a62d4f8f0e1c2e92fbd8dc474665c38bf6429ae3
scorcuera <sc@thehotelsnetwork.com>
2022-05-09 21:42:36 +0200

    feat(problems): add new testing case, whatGeneration

---
+++ src/problems/__tests__/whatGeneration.js
@@ -0,0 +1,13 @@
+import { whatGeneration } from '../whatGeneration';
+
+test('should return \'Generation X\' if year of birth is 1960', () => {
+    expect(whatGeneration(1960)).toBe('Generation X')
+});
+
+test('...', () => {
+    expect(whatGeneration(1989)).toBe('Millennial');
+});
+
+test('should return \'Generation Z\' if year of birth is 2002', () => {
+    
+});
