722aa0b37b11678e35a14894e637f2405ca4c67c
scorcuera <sc@thehotelsnetwork.com>
2022-05-10 20:46:05 +0200

    feat(problems): add testing case, dataSchema

---
+++ src/problems/__tests__/dataSchema.js
@@ -0,0 +1,14 @@
+const data = {
+    name: 'Sergio',
+    coder: true,
+    day: 11,
+    month: 3,
+    year: 1997,
+}
+
+test('should match the expected values\' types', () => {
+    // write your code here
+})
+
+// 1. Write a test that verifies that the properties' values of the data object
+// come in the expected type (e.g. name should be a string, coder should be a Boolean, etc.)
